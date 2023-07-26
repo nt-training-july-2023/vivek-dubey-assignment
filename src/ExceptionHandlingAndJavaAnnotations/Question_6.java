4167e23fa48e8f7c3c8be29d1fbb95e3d1054b96
Vivek Dubey <vivek.dubey@nucleusteq.com>
2023-07-26 22:42:41 +0530

    new files

--- src/ExceptionHandlingAndJavaAnnotations/Question_6.java
+++ src/ExceptionHandlingAndJavaAnnotations/Question_6.java
@@ -1,10 +1,47 @@
 package ExceptionHandlingAndJavaAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Question_6 {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
+        
+        list.add("Hokey");
+        list.add("Cricket");
+        list.add("Football");
+        list.add("Basketball");
+        list.add("Volleyball");
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        
+        System.out.print("Enter the index of the string list you want to access: ");
+        int indexNumber = sc.nextInt();
 
-	}
+        try {
+            
+            String string = fetch(list, indexNumber);
+            System.out.println("String at given index is " + indexNumber + ": " + string);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Error: Index is invalid. The index should be between 0 and " + (list.size() - 1) + ".");
+        } catch (NullPointerException e) {
+            System.err.println("Error: The list is empty. Please provide a list of valid length.");
+        }
+    }
 
+    public static String fetch(List<String> list, int index) throws IndexOutOfBoundsException, NullPointerException {
+        
+
+        if (index >= list.size() ||index < 0  ) {
+            throw new IndexOutOfBoundsException();
+        }
+        
+        if (list == null) {
+            throw new NullPointerException();
+        }
+
+        return list.get(index);
+    }
 }
+
