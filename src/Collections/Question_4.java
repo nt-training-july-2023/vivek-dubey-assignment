b55b373b1e8f748d37e8d9da7d98991012b313ca
Vivek Dubey <vivek.dubey@nucleusteq.com>
2023-08-05 17:10:45 +0530

    Collections

--- src/Collections/Question_4.java
+++ src/Collections/Question_4.java
@@ -1,10 +1,37 @@
 package Collections;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Question_4 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
 
-	}
+        map.put(100, "Vivek");
+        map.put(101, "Morris");
+        map.put(102, "Anderson");
+        map.put(103, "Daniel");
+        
+        Scanner sc = new Scanner(System.in);
 
+        
+        int checkKey = sc.nextInt();
+        if (map.containsKey(checkKey)) {
+            System.out.println("The given Map contains the key: " + checkKey);
+        } else {
+            System.out.println("The given Map does not contain the key: " + checkKey);
+        }
+
+//        sc.next();
+        String valueToCheck = sc.next();
+        if (map.containsValue(valueToCheck)) {
+            System.out.println("The given Map contains value: " + valueToCheck);
+        } else {
+            System.out.println("The given Map does not contain value: " + valueToCheck);
+        }
+        sc.close();
+    }
 }
+
